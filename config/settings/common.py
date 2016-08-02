d523783e070b7e8d54c8070f886fd4214171f4dc
chrisshannon <chris@readymonster.com>
2016-08-02 12:17:25 -0400

    requirements.base.text -Added following dependencies: --django-vanilla-views==1.0.4 --django-bootstrap3==7.0.1 --model-mommy==1.2.6 --webtest==2.0.23 --django-webtest==1.7.9 --django-localflavor==1.3
    config.settings.common
    -Added following THIRD_PARTY_APPS
    --bootstrap3

--- config/settings/common.py
+++ config/settings/common.py
@@ -40,6 +40,7 @@ THIRD_PARTY_APPS = (
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
     'localflavor',
+    'bootstrap3',
 )
 
 # Apps specific for this project go here.
